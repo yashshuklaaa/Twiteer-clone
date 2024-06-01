0d266108bfa2e41b4939f54d320396ad379bf80a
Yash Shukla <shuklayash457@gmail.com>
2024-06-01 09:43:25 +0530

    signup completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Route,Routes} from "react-router-dom";
+import HomePage from "./pages/home/HomePage"
+import LoginPage from "./pages/auth/login/LoginPage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from './components/common/RightPanel';
+import NotificationPage from './pages/notification/NotificationPage';
+import ProfilePage from './pages/profile/ProfilePage';
+import {Toaster} from 'react-hot-toast';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-     <p className ='text-5xl text-red-500 bg-blue-300'>Hello from App</p>
-    </>
+    <div className='flex max-w-6xl mx-auto'>
+      <Sidebar />
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/signup'element ={<SignUpPage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/notifications' element={<NotificationPage />} />
+      <Route path='/profile/:username' element={<ProfilePage />} />
+    </Routes>
+    <RightPanel />
+    <Toaster />
+    </div>
   );
 }
 
